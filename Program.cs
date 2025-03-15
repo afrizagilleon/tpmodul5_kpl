29769211c8aaf833b1e902b6d5bb5a76dfb31dcd
Afriza Gilleon Ginting <afriza.gilleon@gmail.com>
2025-03-15 18:52:09 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
+namespace tpmodul5_103022300154
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            HaloGeneric.SapaUser("Afriza Gilleon");
+        }
+    }
 
-Console.WriteLine("Hello, World!");
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T nama)
+        {
+            Console.WriteLine($"Hello user {nama}");
+        }
+    }
+}

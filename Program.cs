e4402b93eee923e03238e0426938f493dee21b37
Afriza Gilleon Ginting <afriza.gilleon@gmail.com>
2025-03-15 18:57:20 +0700

    Menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
+namespace tpmodul5_103022300154
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
 
-Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class DataGeneric<T>
+    {
+        private T data;
+        DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+        }
+    }
+
+}
